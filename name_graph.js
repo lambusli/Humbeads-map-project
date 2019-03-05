89f73a4f566b65053e06ced68249f3977bc1e02d
Lambus <tianzhil@middlebury.edu>
2019-03-05 09:55:04 -0500

    display names with a big dummy nodes in the middle

--- name_graph.js
+++ name_graph.js
@@ -1,4 +1,4 @@
-const width = 800, height = 800;
+const width = 1000, height = 1000;
 
 const svg = d3.select("#names")
 	.append("svg")
@@ -7,11 +7,28 @@ const svg = d3.select("#names")
 
 const simulation = d3.forceSimulation()
 	.force("center", d3.forceCenter(width / 2, height / 2))
-	.force("charge", d3.forceManyBody())
+	.force("charge", d3.forceManyBody().strength(1))
+	.force("collide", d3.forceCollide())
 
-d3.csv("data/name_sheet_1.csv").then(function(data){
+d3.csv("data/name_sheet_1.csv").then(function(nameList){
+	data = nameList;
 
+	// Prepare extra properties
+	data.forEach(function(d){
+		d.radius = d["Full_name"].length;
+	});
+
+	let size = data.length;
+
+	// Add a big dummy node in the middle
+	data = [{radius: 250}].concat(data);
+
+	// update the display of nodes
 	updateNodes();
+
+	// further configure forceSimulation
+	simulation.force("collide")
+		.radius((d) => d.radius);
 
 	simulation.nodes(data)
 		.on("tick", ticked);
@@ -33,13 +50,12 @@ d3.csv("data/name_sheet_1.csv").then(function(data){
 		let circles = domNodes.append("circle")
 			.attr("cx", 5)
 			.attr("cy", 5)
-			.attr("r", 5)
+			.attr("r", (d) => d.radius)
 			.attr("fill", "#BA7E7A");
 
 		let labels = domNodes.append("text")
 			.classed("label", true)
 			.text((d) => d["Full_name"]);
-
 
 	}
 
